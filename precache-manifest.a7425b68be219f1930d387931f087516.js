2eb4f8baade55b1d997e3cdf199caa59c637c7ba
Vincent Tantra <v_tantra@berkeley.edu>
2020-08-14 17:49:02 -0700

    Updates

---
+++ precache-manifest.a7425b68be219f1930d387931f087516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c0f36c9308fc8250ab7e714a98582e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded199637558ee058d53",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a25c73d7864d0fb78abb",
+    "url": "/static/js/2.6be8c633.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6be8c633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded199637558ee058d53",
+    "url": "/static/js/main.787068b2.chunk.js"
+  },
+  {
+    "revision": "f48fa61dc6a70f71f83a",
+    "url": "/static/js/runtime-main.87c07d1c.js"
+  }
+]);
